73681e56ff886f2d8214c00ee9e9bae1dc6e9273
teyer_klim <alex_artimenko@mail.ru>
2025-10-28 18:45:19 +0500

    nachalo

---
+++ UP_Matsuk/my_project/my_app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Motosalon, MotorTransport, Dealer, Client, Accessory, Service, Deal, DealMotorTransport, DealService, DealAccessory  
+admin.site.register(Motosalon)
+admin.site.register(MotorTransport)
+admin.site.register(Dealer)
+admin.site.register(Client)
+admin.site.register(Accessory)
+admin.site.register(Service)
+admin.site.register(Deal)
+admin.site.register(DealMotorTransport)
+admin.site.register(DealService)
+admin.site.register(DealAccessory)
